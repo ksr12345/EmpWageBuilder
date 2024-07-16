9a626f4c3366d308a04a0c7bb3b79fa2fd17fa5f
ksr12345 <kaustubh.raut@somaiya.edu>
2024-07-16 16:59:06 +0530

    Added: Part time wage using switch case

--- src/EmpWageBuilder.java
+++ src/EmpWageBuilder.java
@@ -1,15 +1,22 @@
 public class EmpWageBuilder {
+    public static final int Full_time = 2;
+    public static final int Part_time = 1;
+    public static final int Emp_Rate = 20;
+
     public static void main(String[] args){
-        int Full_time = 2;
-        int Part_time = 1;
-        int Emp_Rate = 20;
         int emp_hrs = 0;
         int emp_Wage = 0;
-        double emp = Math.floor(Math.random()*10) % 3;
-        if (emp == Full_time){
-            emp_hrs = 8;
-        } else if (emp == Part_time) {
-            emp_hrs =4;
+        int emp = (int)Math.floor(Math.random()*10) % 3;
+        switch (emp){
+            case Full_time:
+                emp_hrs = 8;
+                break;
+            case Part_time:
+                emp_hrs = 4;
+                break;
+            default:
+                emp_hrs = 0;
+                break;
         }
         emp_Wage = emp_hrs*Emp_Rate;
         System.out.println("Emp Wage: "+ emp_Wage);
