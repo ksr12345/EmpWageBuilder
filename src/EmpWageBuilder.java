a5c2fb0201fd056237fefb3af6b1a020878a0318
ksr12345 <kaustubh.raut@somaiya.edu>
2024-07-16 16:50:45 +0530

    Added: Part time wage

--- src/EmpWageBuilder.java
+++ src/EmpWageBuilder.java
@@ -1,12 +1,15 @@
 public class EmpWageBuilder {
     public static void main(String[] args){
-        int Full_time = 1;
+        int Full_time = 2;
+        int Part_time = 1;
         int Emp_Rate = 20;
         int emp_hrs = 0;
         int emp_Wage = 0;
-        double emp = Math.floor(Math.random()*10) % 2;
+        double emp = Math.floor(Math.random()*10) % 3;
         if (emp == Full_time){
             emp_hrs = 8;
+        } else if (emp == Part_time) {
+            emp_hrs =4;
         }
         emp_Wage = emp_hrs*Emp_Rate;
         System.out.println("Emp Wage: "+ emp_Wage);
