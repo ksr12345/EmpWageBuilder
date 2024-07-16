9fec80b38f90074d5f31d9a11868cc5411431be9
ksr12345 <kaustubh.raut@somaiya.edu>
2024-07-16 16:43:36 +0530

    Added: Emp Wage Calculation

--- src/EmpWageBuilder.java
+++ src/EmpWageBuilder.java
@@ -1,11 +1,14 @@
 public class EmpWageBuilder {
     public static void main(String[] args){
         int Full_time = 1;
+        int Emp_Rate = 20;
+        int emp_hrs = 0;
+        int emp_Wage = 0;
         double emp = Math.floor(Math.random()*10) % 2;
         if (emp == Full_time){
-            System.out.println("Employee is present");
-        } else{
-            System.out.println("Employee is absent");
+            emp_hrs = 8;
         }
+        emp_Wage = emp_hrs*Emp_Rate;
+        System.out.println("Emp Wage: "+ emp_Wage);
     }
 }
