136c242131fb4bdffd3a289c12ab903fcadea1a6
ksr12345 <kaustubh.raut@somaiya.edu>
2024-07-16 16:31:02 +0530

    Added: UC1 present or absent

--- src/EmpWageBuilder.java
+++ src/EmpWageBuilder.java
@@ -1,5 +1,11 @@
 public class EmpWageBuilder {
-    public static void main(String[] args) {
-        System.out.println("Welcome to employee wage builder");
+    public static void main(String[] args){
+        int Full_time = 1;
+        double emp = Math.floor(Math.random()*10) % 2;
+        if (emp == Full_time){
+            System.out.println("Employee is present");
+        } else{
+            System.out.println("Employee is absent");
+        }
     }
 }
