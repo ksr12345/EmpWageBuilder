2e14c37a2344497ed3640a74f0b5d0e97ca9482d
ksr12345 <kaustubh.raut@somaiya.edu>
2024-07-17 12:12:25 +0530

    Added: Total wage for each company on different hrs worked

--- src/EmpWageBuilder.java
+++ src/EmpWageBuilder.java
@@ -1,32 +1,36 @@
 public class EmpWageBuilder {
     private final String c_name;
-    private final int rate_hr;
-    private int total;
+    private final double rate_hr;
+    private final int hrs_worked;
+    private double total;
 
-    public EmpWageBuilder(String c_name, int rate_hr){
+    public EmpWageBuilder(String c_name, double rate_hr, int hrs_worked){
         this.c_name = c_name;
         this.rate_hr = rate_hr;
+        this.hrs_worked = hrs_worked;
+        this.total = 0;
     }
 
-    public int find_wage(int hrs_worked){
-        total = rate_hr*hrs_worked;
-        return total;
+    public void find_wage(){
+        double daily_wage = this.hrs_worked*this.rate_hr;
+        this.total += daily_wage;
     }
 
-    public void show_wage(){
-        System.out.println("Total Wage of "+ c_name +" is "+ total);
+    public double show_wage(){
+        return this.total;
     }
 
     public static void main(String[] args){
-        EmpWageBuilder company1 = new EmpWageBuilder("Company 1", 20);
-        EmpWageBuilder company2 = new EmpWageBuilder("Company 2", 18);
+        EmpWageBuilder company1 = new EmpWageBuilder("Company 1", 20.0, 8);
+        EmpWageBuilder company2 = new EmpWageBuilder("Company 2", 25.0, 6);
 
-        int hrs_spent_company1 = 8;
-        company1.find_wage(hrs_spent_company1);
-        company1.show_wage();
+        company1.find_wage();
+        company1.find_wage();
 
-        int hrs_spent_company2 = 9;
-        company2.find_wage(hrs_spent_company2);
-        company2.show_wage();
+        company2.find_wage();
+        company2.find_wage();
+
+        System.out.println("Total wage for " + company1.c_name + " is " + company1.show_wage());
+        System.out.println("Total wage for " + company2.c_name + " is " + company2.show_wage());
     }
 }
