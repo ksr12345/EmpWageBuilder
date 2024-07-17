04b1065de420ce751641b530c90c20cbb38385fd
ksr12345 <kaustubh.raut@somaiya.edu>
2024-07-17 11:55:13 +0530

    Starting with oops

--- src/EmpWageBuilder.java
+++ src/EmpWageBuilder.java
@@ -1,32 +1,13 @@
 public class EmpWageBuilder {
-    public static final int Full_time = 2;
-    public static final int Part_time = 1;
-    public static final int Emp_Rate = 20;
-    public static final int Working_days = 2;
-    public static final int Max_hrs_month = 10;
+    public static int rate_hr = 20;
+
+    public static int find_wage(int hrs_worked){
+        return rate_hr*hrs_worked;
+    }
 
     public static void main(String[] args){
-        int emp_hrs;
-        int total_emp_hrs = 0;
-        int total_emp_days = 0;
-        while (total_emp_hrs <= Max_hrs_month && total_emp_days < Working_days) {
-            total_emp_days ++;
-            int emp = (int) Math.floor(Math.random() * 10) % 3;
-            switch (emp) {
-                case Full_time:
-                    emp_hrs = 8;
-                    break;
-                case Part_time:
-                    emp_hrs = 4;
-                    break;
-                default:
-                    emp_hrs = 0;
-                    break;
-            }
-            total_emp_hrs += emp_hrs;
-            System.out.println("Day#: "+ total_emp_days +" Emp hrs: "+ emp_hrs);
-        }
-        int total_emp_wage = total_emp_hrs * Emp_Rate;
-        System.out.println("Total Employee Wage:" + total_emp_wage);
+        int hrs_spent = 8;
+        int wage = EmpWageBuilder.find_wage(hrs_spent);
+        System.out.println("Employee wage for "+ hrs_spent +" hrs is "+ wage);
     }
 }
