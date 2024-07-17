5404fe535cc78f31dae1c9ae5056052b62822d1b
ksr12345 <kaustubh.raut@somaiya.edu>
2024-07-17 12:07:04 +0530

    Added: Total wage for each company

--- src/EmpWageBuilder.java
+++ src/EmpWageBuilder.java
@@ -1,24 +1,32 @@
 public class EmpWageBuilder {
+    private final String c_name;
     private final int rate_hr;
+    private int total;
 
-    public EmpWageBuilder(int rate_hr){
+    public EmpWageBuilder(String c_name, int rate_hr){
+        this.c_name = c_name;
         this.rate_hr = rate_hr;
     }
 
     public int find_wage(int hrs_worked){
-        return rate_hr*hrs_worked;
+        total = rate_hr*hrs_worked;
+        return total;
+    }
+
+    public void show_wage(){
+        System.out.println("Total Wage of "+ c_name +" is "+ total);
     }
 
     public static void main(String[] args){
-        EmpWageBuilder company1 = new EmpWageBuilder(20);
-        EmpWageBuilder company2 = new EmpWageBuilder(18);
+        EmpWageBuilder company1 = new EmpWageBuilder("Company 1", 20);
+        EmpWageBuilder company2 = new EmpWageBuilder("Company 2", 18);
 
         int hrs_spent_company1 = 8;
-        int wage1 = company1.find_wage(hrs_spent_company1);
-        System.out.println("Employee wage for company1 for "+ hrs_spent_company1 +" hrs is "+ wage1);
+        company1.find_wage(hrs_spent_company1);
+        company1.show_wage();
 
-        int hrs_spent_company2 = 7;
-        int wage2 = company2.find_wage(hrs_spent_company2);
-        System.out.println("Employee wage for company2 for "+ hrs_spent_company2 +" hrs is "+ wage2);
+        int hrs_spent_company2 = 9;
+        company2.find_wage(hrs_spent_company2);
+        company2.show_wage();
     }
 }
